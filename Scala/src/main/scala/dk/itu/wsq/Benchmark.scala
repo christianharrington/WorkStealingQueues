57f18b95a0aee6f6ebb0885bba1a572bd7cc9339
Christian Harrington <christian@harrington.dk>
2014-02-25 13:18:56 +0100

    Added benchmark object.

---
+++ Scala/src/main/scala/dk/itu/wsq/Benchmark.scala
@@ -0,0 +1,38 @@
+package dk.itu.wsq
+
+object Benchmark extends App {
+  import dk.itu.wsq._
+  import dk.itu.wsq.cases.QuickSort 
+  import java.util.Random
+  import scala.collection.mutable.ArrayBuffer
+
+  System.in.read()
+
+  def time[A](f: => A) = {
+    val s = System.nanoTime
+    var ret = f
+    val t = (System.nanoTime - s)/1e6
+
+    println(s"Time: $t ms")
+    ret
+  }
+
+  val l = 100000
+
+  println(s"Generating ArrayBuffer with $l number from 0 to $l ...")
+  val random    = new Random()
+  val testArray = ArrayBuffer.fill(l)(random.nextInt(l))
+  println("Done")
+
+  val wp   = new WorkerPool(QuickSort, 4)
+  val initial = new WorkUnit[ArrayBuffer[Int], ArrayBuffer[Int]](0, None, Seq(testArray))
+  
+  println("Starting...")
+  time(wp.run(Some(initial)))
+  println("Done")
+
+  /*val testArrayBuiltIn = ArrayBuffer.fill(l)(random.nextInt(l))
+  println("Starting...")
+  time(testArrayBuiltIn.sorted)
+  println("Done")*/
+}
