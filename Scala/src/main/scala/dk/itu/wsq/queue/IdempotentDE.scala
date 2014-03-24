bb9209633b7048274742fa4c85b0d1e0586d6807
Christian Harrington <christian@harrington.dk>
2014-03-24 14:34:12 +0100

    Made IdempotentDE more functional :)

--- Scala/src/main/scala/dk/itu/wsq/queue/IdempotentDE.scala
+++ Scala/src/main/scala/dk/itu/wsq/queue/IdempotentDE.scala
@@ -1,16 +1,18 @@
 package dk.itu.wsq.queue
 
 class IdempotentDE[E: Manifest] extends WorkStealingQueue[E] {
-
   import java.util.concurrent.atomic._
+  import scala.annotation.tailrec
 
   case class Anchor(head: Int, size: Int, tag: Int)
 
   private var tasks: Array[E] = new Array[E](1)
 
-  private var anchor: AtomicReference[Anchor] = new AtomicReference[Anchor](Anchor(0, 0, 0)) // (head, size, tag)
+  private var anchor: AtomicReference[Anchor] = 
+    new AtomicReference[Anchor](Anchor(0, 0, 0)) // (head, size, tag)
 
-  def push(e: E): Unit = {
+  @tailrec
+  final def push(e: E): Unit = {
     val localAnchor = anchor.get()
     if(localAnchor.size == tasks.length) {
       expand()
@@ -21,7 +23,7 @@ class IdempotentDE[E: Manifest] extends WorkStealingQueue[E] {
     }
   }
 
-  def take(): Option[E] = {
+  final def take(): Option[E] = {
     val localAnchor = anchor.get()
     if(localAnchor.size == 0) {
       None
@@ -32,24 +34,25 @@ class IdempotentDE[E: Manifest] extends WorkStealingQueue[E] {
     }
   }
 
-  def steal(): Option[E] = {
-    while(true) {
-      val localAnchor = anchor.get()
-      if(localAnchor.size == 0) {
-        return None
-      } else {
-        val arr = tasks
-        val task = arr(localAnchor.head % arr.length)
-        val h2 = localAnchor.head + 1 
-        if(anchor.compareAndSet(localAnchor, Anchor(h2, localAnchor.size - 1, localAnchor.tag))) {
-          return Some(task)
-        }
+  @tailrec
+  final def steal(): Option[E] = {
+    val localAnchor = anchor.get()
+    if(localAnchor.size == 0) {
+      None
+    } else {
+      val arr = tasks
+      val task = arr(localAnchor.head % arr.length)
+      val h2 = localAnchor.head + 1 
+      if(anchor.compareAndSet(localAnchor, Anchor(h2, localAnchor.size - 1, localAnchor.tag))) {
+        Some(task)
+      }
+      else {
+        steal()
       }
     }
-    None
   }
 
-  def expand(): Unit = {
+  final def expand(): Unit = {
     val localAnchor = anchor.get()
     val arr = new Array[E](localAnchor.size * 2)
     for (i <- 0 until localAnchor.size) {
@@ -58,5 +61,5 @@ class IdempotentDE[E: Manifest] extends WorkStealingQueue[E] {
     tasks = arr
   }
 
-  def length: Int = anchor.get().size
+  final def length: Int = anchor.get().size
 }
